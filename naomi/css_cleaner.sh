220a064218e086de01ad5219551c54fa0f7df08b
Datermine <g@datermine.com>
2011-07-05 19:49:52 -0400

    More setup

---
+++ naomi/css_cleaner.sh
@@ -0,0 +1,10 @@
+#/usr/bin/sh
+
+printHeader() {
+  echo "++++++++++++++++++++++++++++++++++++++++"
+  echo $1
+  echo "++++++++++++++++++++++++++++++++++++++++"
+}
+
+printHeader "Cleaning CSS File: $1"
+
