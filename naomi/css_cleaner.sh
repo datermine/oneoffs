4bcd4257d7acb23d059ce07e7161cd9e61ab691f
Datermine <g@datermine.com>
2011-07-05 20:41:32 -0400

    It works

--- naomi/css_cleaner.sh
+++ naomi/css_cleaner.sh
@@ -8,3 +8,19 @@ printHeader() {
 
 printHeader "Cleaning CSS File: $1"
 
+# CP FILE
+newFile="$1.bk"
+cp $1 $newFile
+
+perl -pi -e 's/[\s\t]*line\-height\s*:\s*\d*(\.\d*)?(px|em);[\s\t]*//g' $1
+perl -pi -e 's/[\s\t]*font\-weight\s*:\s*(normal);[\s\t]*//g' $1
+perl -pi -e 's/[\s\t]*font\-style\s*:\s*(normal);[\s\t]*//g' $1
+perl -pi -e 's/[\s\t]*font\-variant\s*:\s*(normal);[\s\t]*//g' $1
+perl -pi -e 's/[\s\t]*text\-align\s*:\s*(left|justify);[\s\t]*//g' $1
+perl -pi -e 's/[\s\t]*text\-transform\s*:\s*(uppercase);[\s\t]*//g' $1
+perl -pi -e 's/[\s\t]*color\s*:\s*(#000000);[\s\t]*//g' $1
+perl -pi -e 's/[\s\t]*text\-decoration\s*:\s*(none);[\s\t]*//g' $1
+perl -pi -e 's/[\s\t]*writing\-mode\s*:\s*(horizontal\-tb);[\s\t]*//g' $1
+
+printHeader "Diffing Starting/Ending Files: $1 $newFile > $1.diff"
+diff $1 $newFile > "$1.diff"
